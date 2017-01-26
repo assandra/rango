2a8f117eae369e4e1f8f1d85cef4da5aaf7bff1f
Assandra Falls <Assandra Falls>
2017-01-26 15:07:45 +0000

    Added likes and views

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,30 @@
-from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# Class for the model representing category
+# which inherit the Model base class
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+# Class for the model representing page
+# which inherit the Model base class
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+
+
+    def __str__(self):
+        return self.title
